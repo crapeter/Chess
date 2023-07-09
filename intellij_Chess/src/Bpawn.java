66b13347c812607eb1e2343a6438ba563852a938
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-09 16:03:45 -0500

    Update Bpawn.java

--- intellij_Chess/src/Bpawn.java
+++ intellij_Chess/src/Bpawn.java
@@ -55,32 +55,26 @@ public class Bpawn implements ActionListener, Images {
     }
 
     public static void diagDisplay(int loc, JButton[] buttons) {
-        if (pieceLoc.containsKey(loc + 7)) {
-            for (String piece : whitePieces) {
+        for (String piece: whitePieces) {
+            if (pieceLoc.containsKey(loc + 7)) {
                 if (pieceLoc.get(loc + 7).equals(piece) && loc % 8 != 0) {
                     buttons[loc + 7].setText("a");
                     buttons[loc + 7].setForeground(Color.red);
                 }
             }
-        }
-        if (pieceLoc.containsKey(loc + 9)) {
-            for (String piece : whitePieces) {
+            if (pieceLoc.containsKey(loc + 9)) {
                 if (pieceLoc.get(loc + 9).equals(piece) && loc % 8 != 7) {
                     buttons[loc + 9].setText("a");
                     buttons[loc + 9].setForeground(Color.red);
                 }
             }
-        }
-        if (pieceLoc.containsKey(loc - 1)) {
-            for (String piece : whitePieces) {
+            if (pieceLoc.containsKey(loc - 1) && loc / 8 == 4) {
                 if (pieceLoc.get(loc - 1).equals(piece) && loc % 8 != 0) {
                     buttons[loc + 7].setText("a");
                     buttons[loc + 7].setForeground(Color.red);
                 }
             }
-        }
-        if (pieceLoc.containsKey(loc + 1)) {
-            for (String piece : whitePieces) {
+            if (pieceLoc.containsKey(loc + 1) && loc / 8 == 4) {
                 if (pieceLoc.get(loc + 1).equals(piece) && loc % 8 != 7) {
                     buttons[loc + 9].setText("a");
                     buttons[loc + 9].setForeground(Color.red);
