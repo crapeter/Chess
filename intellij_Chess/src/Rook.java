5360345a465c7997159a68ddb253e2f696bd109a
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-17 14:15:53 -0500

    Delete intellij_Chess directory

--- intellij_Chess/src/Rook.java
+++
@@ -1,6 +0,0 @@
-import java.util.HashMap;
-import javax.swing.JButton;
-
-public class Rook extends RBQMoves {
-    public static void move(int location, JButton[] buttons, HashMap<Integer, String> pieceLoc) { move1(location, buttons, pieceLoc); }
-}
