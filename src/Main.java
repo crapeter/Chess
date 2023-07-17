2e810035bbaabc07dfa69403be9e1c8021cdfd7d
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-17 15:21:46 -0500

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main implements setupVars{
-    public static void main(String[] args) {
-        BuildChess pieces = new BuildChess();
-        Draw board = new Draw();
-        board.draw();
-        pieces.placePieces();
-    }
-}
--- src/Queen.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import javax.swing.JButton;
-
-public class Queen extends RBQMoves {
-    public static void move(int location, JButton[] buttons, HashMap<Integer, String> pieceLoc) {
-        move1(location, buttons, pieceLoc);
-        move2(location, buttons, pieceLoc);
-    }
-}
