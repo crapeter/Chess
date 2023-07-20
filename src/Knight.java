0ae65212d581be09f787d3cb704b20e0d5347648
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-20 16:49:34 -0500

    Update Knight.java

--- src/Knight.java
+++ src/Knight.java
@@ -4,23 +4,15 @@ import javax.swing.JButton;
 
 public class Knight extends HelperFunctions {
     public static void move(int location, JButton[] buttons, HashMap<Integer, String> pieceLoc) {
-        int loc1 = location - 17;
-        int loc2 = location - 15;
-        int loc3 = location - 10;
-        int loc4 = location - 6;
-        int loc5 = location + 6;
-        int loc6 = location + 10;
-        int loc7 = location + 15;
-        int loc8 = location + 17;
-
-        canPlace(loc1, location, 2, buttons, pieceLoc);
-        canPlace(loc2, location, 2, buttons, pieceLoc);
-        canPlace(loc3, location, 1, buttons, pieceLoc);
-        canPlace(loc4, location, 1, buttons, pieceLoc);
-        canPlace(loc5, location, 1, buttons, pieceLoc);
-        canPlace(loc6, location, 1, buttons, pieceLoc);
-        canPlace(loc7, location, 2, buttons, pieceLoc);
-        canPlace(loc8, location, 2, buttons, pieceLoc);
+        int[] moveOptions = {location - 17, location - 15, location - 10, location - 6,
+                             location + 6, location + 10, location + 15, location + 17};
+        for (int i : moveOptions) {
+            if (i == location - 10 || i == location - 6 || i == location + 6 || i == location + 10) {
+                canPlace(i, location, 1, buttons, pieceLoc);
+            } else {
+                canPlace(i, location, 2, buttons, pieceLoc);
+            }
+        }
     }
     public static void canPlace(int loc, int location, int spaceBetween, JButton[] buttons, HashMap<Integer, String> pieceLoc) {
         if (0 <= loc && loc < 64 && Math.abs((location / 8) - (loc / 8)) == spaceBetween)
