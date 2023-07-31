4133c1201fa9f4149fdc167883c04edc18f629fc
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-31 09:49:26 -0500

    Update Images.java

--- intellij_Chess/src/Images.java
+++ intellij_Chess/src/Images.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
 
 public interface Images {
-    ImageIcon bBishop = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black Bishop.png");
-    ImageIcon bKing = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black King.png");
-    ImageIcon bKnight = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black Knight.png");
-    ImageIcon bPawn = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black Pawn.png");
-    ImageIcon bQueen = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black Queen.png");
-    ImageIcon bRook = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/Black Rook.png");
-    ImageIcon wBishop = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White Bishop.png");
-    ImageIcon wKing = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White King.png");
-    ImageIcon wKnight = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White Knight.png");
-    ImageIcon wPawn = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White Pawn.png");
-    ImageIcon wQueen = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White Queen.png");
-    ImageIcon wRook = new ImageIcon("C:/Users/craig/IdeaProjects/intellij_Chess/src/Images/White Rook.png");
+    ImageIcon bBishop = new ImageIcon("src/Images/Black Bishop.png");
+    ImageIcon bKing = new ImageIcon("src/Images/Black King.png");
+    ImageIcon bKnight = new ImageIcon("src/Images/Black Knight.png");
+    ImageIcon bPawn = new ImageIcon("src/Images/Black Pawn.png");
+    ImageIcon bQueen = new ImageIcon("src/Images/Black Queen.png");
+    ImageIcon bRook = new ImageIcon("src/Images/Black Rook.png");
+    ImageIcon wBishop = new ImageIcon("src/Images/White Bishop.png");
+    ImageIcon wKing = new ImageIcon("src/Images/White King.png");
+    ImageIcon wKnight = new ImageIcon("src/Images/White Knight.png");
+    ImageIcon wPawn = new ImageIcon("src/Images/White Pawn.png");
+    ImageIcon wQueen = new ImageIcon("src/Images/White Queen.png");
+    ImageIcon wRook = new ImageIcon("src/Images/White Rook.png");
 }
