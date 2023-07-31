195a387d90417d2b313e2b1ec6c4a07e44372946
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-31 11:16:42 -0500

    Delete intellij_Chess directory

--- intellij_Chess/src/Bishop.java
+++
@@ -1,3 +0,0 @@
-public class Bishop extends RBQMoves {
-    public static void move() { move2(location, buttons, pieceLoc); }
-}
--- intellij_Chess/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        BuildChess pieces = new BuildChess();
-        Draw board = new Draw();
-        board.draw();
-        pieces.place();
-    }
-}
