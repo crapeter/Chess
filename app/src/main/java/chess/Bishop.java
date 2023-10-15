e3fe28f7bc3f28aa46000cadc90289eb6df9d8da
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-10-15 12:24:05 -0500

    Delete app directory

--- app/src/main/java/chess/Bishop.java
+++
@@ -1,7 +0,0 @@
-package chess;
-
-public class Bishop extends RBQMoves {
-  public static void display() {
-    move2();
-  }
-}
--- app/src/main/java/chess/Queen.java
+++
@@ -1,8 +0,0 @@
-package chess;
-
-public class Queen extends RBQMoves {
-  public static void display() {
-    move1();
-    move2();
-  }
-}
