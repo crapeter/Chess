199f5891bf38fa09c6df1f338d885abb7a665b30
crapeter <craig.peterson0701@gmail.com>
2024-03-07 18:18:58 -0600

    Got rid of the 'welcome to chess' message in App class

--- src/main/java/chess/App.java
+++ src/main/java/chess/App.java
@@ -1,12 +1,8 @@
 package chess;
 
 public class App {
-  public String getGreeting() {
-    return "Welcome to jSwing Chess";
-  }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
     PieceFunctionality piece = new PieceFunctionality();
     PieceSetup pieces = new PieceSetup();
     Draw board = new Draw();
